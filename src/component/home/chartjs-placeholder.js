52a4cd18ab9e90526d05c7ecf21d1d0febd0ca22
gouthamguna <ggowthamsankar@gmail.com>
2025-10-03 03:21:49 +0530

    remove the un-Impl func.!

--- src/component/home/chartjs-placeholder.js
+++
@@ -1,4 +0,0 @@
-/* Simple Chart.js CDN for Angular (for demo purposes) */
-// This file demonstrates initializing a Chart.js chart for demo purposes.
-
-// Make sure Chart.js is loaded via CDN in your HTML before this script runs.
