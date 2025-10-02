94274d6e14d2c60f8cc17919fbc7c90ae258040f
gouthamguna <ggowthamsankar@gmail.com>
2025-10-03 03:39:40 +0530

    Updated stands.!

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,12 @@
 import { Component } from '@angular/core';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { Header} from '../component/header/header';
-import { Footer } from '../component/footer/footer';
-import { filter } from 'rxjs/operators';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet],
+  template: '<router-outlet />',
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'angular-app';
-  showHeaderAndFooter = true;
-
-  constructor(private router: Router) {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe(() => {
-      const route = this.router.routerState.snapshot.root.firstChild;
-      this.showHeaderAndFooter = route?.data?.['layout'] !== 'empty';
-    });
-  }
 }
